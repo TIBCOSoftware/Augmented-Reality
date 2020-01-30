c0e87008890f9031769addca2786aef8acde3b4a
Jörg Grote <joerg@grote-online.de>
2020-01-30 16:41:51 +0100

    init Nimbus
    first Version

---
+++ sources/TIBCO-Nimbus/Nimbus-Basic/Assets/EditorPlayModeEnabler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enables Objects just in Editor Play Mode
+namespace TIBCOLabsToolkit
+{
+    public class EditorPlayModeEnabler : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        public GameObject background;
+
+        void Start()
+        {
+            background.SetActive(true);
+        }
+#endif
+    }
+}
